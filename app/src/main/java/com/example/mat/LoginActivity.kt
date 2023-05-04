c3f523bf0d56b0666e9f585d42a4dcbac0858ccf
Rahul Banjara <116911858+SandytheDeveloper@users.noreply.github.com>
2023-05-04 23:32:12 +0530

    Rahul
    first commit by Rahul

--- app/src/main/java/com/example/mat/LoginActivity.kt
+++ app/src/main/java/com/example/mat/LoginActivity.kt
@@ -3,10 +3,9 @@ package com.example.mat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-         print("Hello")
+        setContentView(R.layout.activity_login)
     }
 }
